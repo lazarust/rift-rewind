51e219a21fd0dbe2362dd3863b5ed62c824e459d
Thomas Lazarus <lazarustda@gmail.com>
2025-09-24 09:28:41 -0500

    Initial MCP server

---
+++ riot_mcp.py
@@ -0,0 +1,42 @@
+import os
+
+import httpx
+from dotenv import load_dotenv
+from mcp.server.fastmcp import FastMCP
+
+load_dotenv()
+mcp = FastMCP("riot")
+base_url = "https://americas.api.riotgames.com"
+
+
+async def make_riot_request(url: str):
+    headers = {"X-Riot-Token": os.getenv("RIOT_API_KEY")}
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url, headers=headers, timeout=30.0)
+            response.raise_for_status()
+            return response.json()
+        except Exception:
+            return None
+
+
+@mcp.tool()
+async def get_puuid_by_summoner(summoner_name: str, summoner_tagline: str) -> str:
+    """Get the puuid of a summoner by summoner name and tagline
+
+    Args:
+        summoner_name: Summoner Name
+        summoner_tagline: Summoner Tagline
+    """
+    url = f"{base_url}/riot/account/v1/accounts/by-riot-id/{summoner_name}/{summoner_tagline}"
+    response = await make_riot_request(url)
+
+    if not response:
+        return "Bad Response"
+
+    return response["puuid"]
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
