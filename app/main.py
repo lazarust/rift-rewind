efed517966bfa915daf28821673e776d0ed517cf
Thomas Lazarus <lazarustda@gmail.com>
2025-10-02 09:05:47 -0500

    Gets Gradio with Bedrock working

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import logging
 
+import boto3
 import gradio as gr
 
 logging.basicConfig(
@@ -10,9 +11,24 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+model_id = "amazon.nova-micro-v1:0"
+client = boto3.client("bedrock-runtime", region_name="us-east-1")
+
 
 def answer(message, history):
-    return f"History Len: {len(history)}\n Message: {message}"
+    messages = history.copy()
+    print(messages)
+    current_message = {"role": "user", "content": [{"text": message}]}
+    messages.append(current_message)
+    try:
+        response = client.converse(
+            modelId=model_id,
+            messages=messages,
+            inferenceConfig={"maxTokens": 512, "temperature": 0.5, "topP": 0.9},
+        )
+        return response["output"]["message"]["content"][0]["text"]
+    except Exception as e:
+        return str(e)
 
 
 def main():
